6f68bedc58b0fe456af21bfbba3f42f38c1e4bb6
anshBhatia <anshbhatia20@gmail.com>
2025-06-17 23:00:01 +0530

    leftover stuff

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -4,7 +4,7 @@ const PlayerInfo = ({ player, emoji, isActive }) => {
   return (
     <div className={`player-card ${isActive ? 'active' : ''}`}>
       <div className="player-emoji">{emoji}</div>
-      <div className="player-name">{player}</div>
+      <div className="player-name"> {player}</div>
     </div>
   );
 };
